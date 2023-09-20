395215ae1afb30ede77203af2f6482bf9a10c931
Misha Bhargava <mishabhargava@gmail.com>
2023-09-20 14:44:41 +0530

    Changes2

---
+++ app/src/main/java/com/example/slcm/GradeActivity.java
@@ -0,0 +1,30 @@
+package com.example.slcm;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GradeActivity extends AppCompatActivity {
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_grade);
+
+    }
+}
