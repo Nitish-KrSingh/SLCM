e54a6b4ff3341b35dfe05ddb2896be07cd2c55b1
Ajoy10 <achu.ajoy10@gmail.com>
2023-09-20 15:24:56 +0530

    Adding all activity

--- app/src/main/java/com/example/slcm/ChooseDateActivity.java
+++ app/src/main/java/com/example/slcm/ChooseDateActivity.java
@@ -3,6 +3,9 @@ package com.example.slcm;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class ChooseDateActivity extends AppCompatActivity {
 
@@ -10,5 +13,29 @@ public class ChooseDateActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose_date);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Select Date");
+
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_ham, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item1) {
+            Toast.makeText(this, "Clicked on about ", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.menu_item2) {
+            Toast.makeText(this, "Clicked on setting ", Toast.LENGTH_SHORT).show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+
+
     }
 }
--- app/src/main/java/com/example/slcm/MarksActivity.java
+++ app/src/main/java/com/example/slcm/MarksActivity.java
@@ -1,7 +1,10 @@
 package com.example.slcm;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,7 +16,8 @@ public class MarksActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_marks);
-
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Marks");
         listView = findViewById(R.id.students);
 
         // Sample student data
@@ -25,5 +29,27 @@ public class MarksActivity extends AppCompatActivity {
 
         CustomStudentListAdapter adapter = new CustomStudentListAdapter(this, students);
         listView.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_ham, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.menu_item1) {
+            Toast.makeText(this, "Clicked on about ", Toast.LENGTH_SHORT).show();
+        } else if (id == R.id.menu_item2) {
+            Toast.makeText(this, "Clicked on setting ", Toast.LENGTH_SHORT).show();
+        }
+
+
+        return super.onOptionsItemSelected(item);
+
+
     }
 }
