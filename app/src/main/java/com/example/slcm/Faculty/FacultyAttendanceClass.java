388cfcc369a75c65f9c189e4bb8fe8755e999c43
nitish <nitishbcps@gmail.com>
2023-10-14 01:27:17 +0530

    Attendance process select class , select date , select subject

--- app/src/main/java/com/example/slcm/Faculty/FacultyAttendanceClass.java
+++ app/src/main/java/com/example/slcm/Faculty/FacultyAttendanceClass.java
@@ -2,15 +2,28 @@ package com.example.slcm.Faculty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
+import com.example.slcm.DatabaseManager;
 import com.example.slcm.R;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 public class FacultyAttendanceClass extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +31,38 @@ public class FacultyAttendanceClass extends AppCompatActivity {
         setContentView(R.layout.activity_faculty_attendance_class);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Attendance - Select Class");
+
+        DatabaseManager db = new DatabaseManager(this);
+
+        String selectedDate = getIntent().getStringExtra("selectedDate");
+
+        SharedPreferences sharedPreferences = getSharedPreferences("login_state", Context.MODE_PRIVATE);
+        String fac_name = sharedPreferences.getString("LOGIN_USER","");
+
+
+        List<String> assignedClasses = db.getClassesAssignedToFaculty(fac_name);
+
+
+
+        ArrayAdapter<String> classAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, assignedClasses);
+        ListView classListView = findViewById(R.id.classListView);
+        classListView.setAdapter(classAdapter);
+
+
+        classListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedClass = assignedClasses.get(position);
+                Intent intent = new Intent(FacultyAttendanceClass.this , FacultyAttendanceSubject.class);
+                intent.putExtra("class_name" , selectedClass);
+                intent.putExtra("s_date" , selectedDate);
+
+                startActivity(intent);
+            }
+        });
+
+
+
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
