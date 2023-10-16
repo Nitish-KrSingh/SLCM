e3e1a6fa35eb0407b196763e97d3300ba9952487
shashikantaa <98572169+shashikantaa@users.noreply.github.com>
2023-10-16 15:41:55 +0530

    Add files via upload

--- app/src/main/java/com/example/slcm/Faculty/FacultyTimetable.java
+++ app/src/main/java/com/example/slcm/Faculty/FacultyTimetable.java
@@ -1,16 +1,24 @@
 package com.example.slcm.Faculty;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
+import android.widget.ImageView;
 
 import com.example.slcm.R;
+import com.example.slcm.Student.StudentTimetable;
 
 import java.util.Objects;
 
 public class FacultyTimetable extends AppCompatActivity {
+    private ImageView imageView;
+    private ScaleGestureDetector scaleGestureDetector;
+    private float FACTOR = 1.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +26,28 @@ public class FacultyTimetable extends AppCompatActivity {
         setContentView(R.layout.activity_faculty_timetable);
         Objects.requireNonNull(getSupportActionBar()).setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle("Time Table");
+        imageView = (ImageView) findViewById(R.id.faculty_time_table);
+        scaleGestureDetector = new ScaleGestureDetector(this, new FacultyTimetable.Scalelistner());
     }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        scaleGestureDetector.onTouchEvent(event);
+        return super.onTouchEvent(event);
+    }
+
+    class Scalelistner extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(@NonNull ScaleGestureDetector detector) {
+            FACTOR *= detector.getScaleFactor();
+            FACTOR = Math.max(0.1f, Math.min(FACTOR, 10.f));
+            imageView.setScaleX(FACTOR);
+            imageView.setScaleY(FACTOR);
+            return true;
+        }
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.faculty_menu, menu);
